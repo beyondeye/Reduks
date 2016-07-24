abe918edad8b49be5f7165c628e264dbd9cdf243
beyondeye <dario-e@beyond-eye.com>
2016-07-24 11:22:39 +0300

    Constants and NodeType converted to kotlin

---
+++ zjsonpatch/src/main/kotlin/com/beyondeye/zjsonpatch/Constants.kt
@@ -0,0 +1,15 @@
+package com.beyondeye.zjsonpatch
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gopi.vishwakarma
+ * Date: 10/07/15
+ * Time: 10:35 AM
+ */
+internal object Constants {
+    var OP = "op"
+    var VALUE = "value"
+    var PATH = "path"
+    var FROM = "from"
+
+}
