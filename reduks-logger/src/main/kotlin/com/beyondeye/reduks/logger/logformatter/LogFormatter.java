d89efd76a1c5dd0d61a7a4e5557d779b73a3d206
beyondeye <dario-e@beyond-eye.com>
2016-07-26 14:57:27 +0300

    removed commented code from LogFormatter

--- reduks-logger/src/main/kotlin/com/beyondeye/reduks/logger/logformatter/LogFormatter.java
+++ reduks-logger/src/main/kotlin/com/beyondeye/reduks/logger/logformatter/LogFormatter.java
@@ -4,14 +4,10 @@ package com.beyondeye.reduks.logger.logformatter;
  * LogFormatter is a wrapper of {@link android.util.Log}
  * But more pretty, simple and powerful
  * adapted from https://github.com/orhanobut/logger
+ * It also support log entry grouping i.e. common indenting of multiple subsequent lines in a similar way to javascript console:
+ * see https://developer.mozilla.org/en/docs/Web/API/console (Using groups in console)
  */
 public class LogFormatter {
-//  public static final int DEBUG = 3;
-//  public static final int ERROR = 6;
-//  public static final int ASSERT = 7;
-//  public static final int INFO = 4;
-//  public static final int VERBOSE = 2;
-//  public static final int WARN = 5;
 
   private final LogFormatterPrinter printer;
 
@@ -24,17 +20,11 @@ public class LogFormatter {
   }
   public LogFormatterSettings getSettings() { return printer.getSettings(); }
 
-//  public LogFormatterPrinter t(String tag) {
-//    return printer.t(tag, printer.getSettings().getMethodCount());
-//  }
 
   public LogFormatterPrinter t(int methodCount) {
     return printer.t(methodCount);
   }
 
-//  public LogFormatterPrinter t(String tag, int methodCount) {
-//    return printer.t(tag, methodCount);
-//  }
   public void groupStart() {
     printer.groupStart();
   }
@@ -48,38 +38,6 @@ public class LogFormatter {
   public  void log(int loglevel, String tagSuffix, String message, Throwable throwable) {
     printer.log(loglevel, tagSuffix, message, throwable);
   }
-
-//  public static void d(String message, Object... args) {
-//    printer.d(message, args);
-//  }
-
-//  public static void d(Object object) {
-//    printer.d(object);
-//  }
-
-//  public static void e(String message, Object... args) {
-//    printer.e(null, message, args);
-//  }
-
-//  public static void e(Throwable throwable, String message, Object... args) {
-//    printer.e(throwable, message, args);
-//  }
-
-//  public static void i(String message, Object... args) {
-//    printer.i(message, args);
-//  }
-//
-//  public static void v(String message, Object... args) {
-//    printer.v(message, args);
-//  }
-//
-//  public static void w(String message, Object... args) {
-//    printer.w(message, args);
-//  }
-//
-//  public static void wtf(String message, Object... args) {
-//    printer.wtf(message, args);
-//  }
 
   /**
    * Formats the json content and print it
