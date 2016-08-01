b95a07e9ab58f19b51852d0124d03be8c13a341c
beyondeye <dario-e@beyond-eye.com>
2016-08-01 09:30:52 +0300

    wrap and unwrap middleware renamed

--- reduks/src/main/kotlin/com/beyondeye/reduks/middlewares/UnwrapActionMiddleware.kt
+++ reduks/src/main/kotlin/com/beyondeye/reduks/middlewares/UnwrapActionMiddleware.kt
@@ -1,7 +1,6 @@
 package com.beyondeye.reduks.middlewares
 
 import com.beyondeye.reduks.*
-import com.beyondeye.reduks.modules.ActionContext
 import com.beyondeye.reduks.modules.ActionWithContext
 
 
@@ -9,10 +8,10 @@ import com.beyondeye.reduks.modules.ActionWithContext
  * A middleware for unwrapping an action that has some module context
  */
 
-class WrapActionWithContextMiddleware<S>(val actionContext:ActionContext) : Middleware<S> {
+class UnwrapActionMiddleware<S> : Middleware<S> {
     override fun dispatch(store: Store<S>, next: NextDispatcher, action: Any):Any {
-        if(action !is ActionWithContext) {
-            return next(ActionWithContext(action,actionContext))
+        if(action is ActionWithContext) {
+            return next(action.action)
         }
         return  next(action)
     }
--- reduks/src/main/kotlin/com/beyondeye/reduks/middlewares/WrapActionMiddleware.kt
+++ reduks/src/main/kotlin/com/beyondeye/reduks/middlewares/WrapActionMiddleware.kt
@@ -1,6 +1,7 @@
 package com.beyondeye.reduks.middlewares
 
 import com.beyondeye.reduks.*
+import com.beyondeye.reduks.modules.ActionContext
 import com.beyondeye.reduks.modules.ActionWithContext
 
 
@@ -8,10 +9,10 @@ import com.beyondeye.reduks.modules.ActionWithContext
  * A middleware for unwrapping an action that has some module context
  */
 
-class UnwrapActionWithContextMiddleware<S> : Middleware<S> {
+class WrapActionMiddleware<S>(val actionContext:ActionContext) : Middleware<S> {
     override fun dispatch(store: Store<S>, next: NextDispatcher, action: Any):Any {
-        if(action is ActionWithContext) {
-            return next(action.action)
+        if(action !is ActionWithContext) {
+            return next(ActionWithContext(action,actionContext))
         }
         return  next(action)
     }
