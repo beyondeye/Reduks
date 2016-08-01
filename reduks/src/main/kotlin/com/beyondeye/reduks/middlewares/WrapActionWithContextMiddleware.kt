0154f2fc7872ab7eab29ac89355fa5b16102e227
beyondeye <dario-e@beyond-eye.com>
2016-08-01 09:23:18 +0300

    added WrapActionWithContextMiddleware

---
+++ reduks/src/main/kotlin/com/beyondeye/reduks/middlewares/WrapActionWithContextMiddleware.kt
@@ -0,0 +1,20 @@
+package com.beyondeye.reduks.middlewares
+
+import com.beyondeye.reduks.*
+import com.beyondeye.reduks.modules.ActionContext
+import com.beyondeye.reduks.modules.ActionWithContext
+
+
+/**
+ * A middleware for unwrapping an action that has some module context
+ */
+
+class WrapActionWithContextMiddleware<S>(val actionContext:ActionContext) : Middleware<S> {
+    override fun dispatch(store: Store<S>, next: NextDispatcher, action: Any):Any {
+        if(action !is ActionWithContext) {
+            return next(ActionWithContext(action,actionContext))
+        }
+        return  next(action)
+    }
+
+}
