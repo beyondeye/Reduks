c5da1ee7943b0149bb650c1faf4731b0031b85a7
beyondeye <dario-e@beyond-eye.com>
2016-08-16 10:44:31 +0300

    updated readme: subscribers, actions and reducers

---
+++ examples/src/main/java/beyondeye/com/examples/LoginInfo.kt
@@ -0,0 +1,6 @@
+package beyondeye.com.examples
+
+/**
+ * Created by daely on 8/16/2016.
+ */
+class LoginInfo(val email: String, val password: String)
---
+++ examples/src/main/java/beyondeye/com/examples/login/reduks/subscriber.kt
@@ -0,0 +1,35 @@
+package beyondeye.com.examples.login.reduks
+
+import beyondeye.com.examples.LoginInfo
+import com.beyondeye.reduks.SelectorBuilder
+import com.beyondeye.reduks.StoreSubscriber
+import com.beyondeye.reduks.StoreSubscriberBuilder
+
+/**
+ * Created by daely on 8/16/2016.
+ */
+val curLogInfo=LoginInfo("","")
+val subscriber1=StoreSubscriber<ActivityState> { newState ->
+    val loginfo=LoginInfo(newState.email,newState.password)
+    if(loginfo.email!= curLogInfo.email||loginfo.password!= curLogInfo.password) {
+        //log info changed...send to server for verification
+
+    }
+}
+
+val subscriberBuilder = StoreSubscriberBuilder<ActivityState> { store ->
+    val sel = SelectorBuilder<ActivityState>()
+    val sel4LoginInfo=sel.withField { email } .withField { password }.compute { e, p -> LoginInfo(e,p)  }
+    val sel4email=sel.withSingleField { email }
+    StoreSubscriber { newState ->
+        sel4LoginInfo.onChangeIn(newState) { newLogInfo ->
+            //log info changed...send to server for verification
+            //...then we received notification that email was verified
+            store.dispatch(Action.EmailConfirmed())
+        }
+        sel4email.onChangeIn(newState) { newEmail ->
+            //email changed : do something
+        }
+
+    }
+}
