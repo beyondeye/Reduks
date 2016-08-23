6fb72175b8b1c21862edf93463205eebe3f6f639
beyondeye <dario-e@beyond-eye.com>
2016-08-23 18:28:57 +0300

    updated java docs

--- reduks/src/main/java/com/beyondeye/reduks/StoreEnhancer.java
+++ reduks/src/main/java/com/beyondeye/reduks/StoreEnhancer.java
@@ -3,6 +3,8 @@ package com.beyondeye.reduks;
 /**
  * get a store creator and return a new enhanced one
  * see https://github.com/reactjs/redux/blob/master/docs/Glossary.md#store-enhancer
+ *
+ * keep this interface in java to leverage kotlin SAM conversion (lambda automatical converted to implementation of java interface)
  * Created by daely on 8/23/2016.
  */
 public interface StoreEnhancer<S> {
