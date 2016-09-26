6695be0fe4fd68645af09b1fd1a14ce81a61ce3b
Tipatai Puthanukunkit <judrummercpe@gmail.com>
2016-09-26 19:48:22 +0700

    fix subscribe extension to return subscription (#13)

--- reduks/src/main/kotlin/com/beyondeye/reduks/Store.kt
+++ reduks/src/main/kotlin/com/beyondeye/reduks/Store.kt
@@ -27,17 +27,16 @@ interface Store<S> {
 /**
  * extension method for directly provide a lambda as argument for store subscribe
  */
-fun <S>Store<S>.subscribe(lambda:()->Unit) {
-    this.subscribe(StoreSubscriber<S>{lambda()})
-}
+fun <S> Store<S>.subscribe(lambda: () -> Unit) = this.subscribe(StoreSubscriber<S> { lambda() })
+
 /**
  * extension method for checking at compile time that we only dispatch objects derived from
  * base [Action] interface
  */
-fun <S>Store<S>.dispatch_a(action:Action)=dispatch(action)
+fun <S> Store<S>.dispatch_a(action: Action) = dispatch(action)
 
 /**
  * extension method for checking at compile time that we only dispatch objects derived from
  * base [StandardAction] interface
  */
-fun <S>Store<S>.dispatch_sa(action:StandardAction)=dispatch(action)
+fun <S> Store<S>.dispatch_sa(action: StandardAction) = dispatch(action)
