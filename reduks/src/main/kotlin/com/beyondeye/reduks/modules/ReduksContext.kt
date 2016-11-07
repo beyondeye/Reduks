fd9c2e717883366ad2fed7bd8a354ae788c7e1f6
beyondeye <dario-e@beyond-eye.com>
2016-11-07 14:30:56 +0200

    added .. operator for creating action with context

--- reduks/src/main/kotlin/com/beyondeye/reduks/modules/ReduksContext.kt
+++ reduks/src/main/kotlin/com/beyondeye/reduks/modules/ReduksContext.kt
@@ -5,5 +5,13 @@ package com.beyondeye.reduks.modules
  * Created by daely on 7/31/2016.
  */
 class ReduksContext(val moduleId:String) {
+    /**
+     * compose two contexts
+     */
     infix operator fun plus(other: ReduksContext) = ReduksContext("$moduleId+${other.moduleId}")
+    /**
+     * generate action with context with '..' operator
+     * E.g. ctx..Action()
+     */
+    operator fun rangeTo(action:Any) = ActionWithContext(action,this)
 }
