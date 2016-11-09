1d5fbab9b310ffd3e06d01f33e8ed4e252c036d6
beyondeye <dario-e@beyond-eye.com>
2016-11-09 14:46:37 +0200

    added explanation in javadoc why StateWithBusData is an interface not an abstract class

--- reduks-bus/src/main/kotlin/com/beyondeye/reduks/bus/StateWithBusData.kt
+++ reduks-bus/src/main/kotlin/com/beyondeye/reduks/bus/StateWithBusData.kt
@@ -5,6 +5,10 @@ import com.beyondeye.reduks.pcollections.PMap
 
 /**
  * base interface for reduks State class that can handle bus data
+ * NOTE: it would be much easier to to have StateWithBusData be an abstract Class, so that
+ *       there would be no need to override busData with a concrete implementation, but since
+ *       data classes cannot inherit except from interfaces (at least in kotlin 1.0.x), this
+ *       choice would forbid us to use data classes as state with busdata
  * Created by daely on 9/30/2016.
  */
 interface StateWithBusData {
