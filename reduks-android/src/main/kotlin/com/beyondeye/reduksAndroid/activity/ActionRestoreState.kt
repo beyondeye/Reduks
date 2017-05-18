356f95494c067abbf5822d1e96b084b01d06fbc0
beyondeye <dario-e@beyond-eye.com>
2017-05-18 21:52:03 +0300

    added support for Serializable reduks state in automatic save and restore in OnSaveInstanceState/OnRestoreInstanceState

--- reduks-android/src/main/kotlin/com/beyondeye/reduksAndroid/activity/ActionRestoreState.kt
+++ reduks-android/src/main/kotlin/com/beyondeye/reduksAndroid/activity/ActionRestoreState.kt
@@ -5,6 +5,7 @@ import android.os.Parcelable
 import com.beyondeye.reduks.Action
 import com.beyondeye.reduks.ReducerFn
 import com.beyondeye.reduks.Reduks
+import java.io.Serializable
 
 class ActionRestoreState<S>(val restoredState:S): Action {
     companion object {
@@ -17,17 +18,18 @@ class ActionRestoreState<S>(val restoredState:S): Action {
         }
         fun<S> saveReduksState(reduks: Reduks<S>, outState: Bundle?) {
             val curReduksState=reduks.store.state
-            if(curReduksState is Parcelable) {
-                outState?.putParcelable(REDUKS_STATE,curReduksState)
+            when(curReduksState) {
+                is Parcelable -> outState?.putParcelable(REDUKS_STATE,curReduksState)
+                is Serializable-> outState?.putSerializable(REDUKS_STATE,curReduksState)
             }
         }
         fun<S> restoreReduksState(reduks: Reduks<S>, savedInstanceState: Bundle?) {
-            if(reduks.store.state is Parcelable) {
-                val restoredState:S?=savedInstanceState?.getParcelable<Parcelable>(REDUKS_STATE) as S?
-                if(restoredState!=null) {
-                    reduks.store.dispatch(ActionRestoreState(restoredState))
-                }
+            val restoredState:S? = when(reduks.store.state) {
+                is Parcelable ->savedInstanceState?.getParcelable<Parcelable>(REDUKS_STATE) as? S
+                is Serializable -> savedInstanceState?.getSerializable(REDUKS_STATE) as? S
+                else -> null
             }
+            restoredState?.let { reduks.store.dispatch(ActionRestoreState(it)) }
         }
         private val REDUKS_STATE: String?="REDUKS_STATE"
 
