b45d555002e498f39ff2bc2040a26dd168dcbb48
beyondeye <dario-e@beyond-eye.com>
2017-12-22 13:06:42 +0200

    added javadocs

--- reduks/src/main/kotlin/com/beyondeye/reduks/Action.kt
+++ reduks/src/main/kotlin/com/beyondeye/reduks/Action.kt
@@ -2,6 +2,7 @@ package com.beyondeye.reduks
 
 /**
  * base interface for all standard Reduks actions
+ * (NOT all actions, only Reduks standard actions)
  * Created by daely on 8/28/2016.
  */
 interface Action
