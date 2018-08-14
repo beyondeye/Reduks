74456250b2c876a20a4fe01208d6b238410785fd
beyondeye <dario-e@beyond-eye.com>
2018-08-14 11:19:09 +0300

    DispatcherFn now keep a strong reference by default to dispatch method, because weak reference caused problems

--- reduks/src/main/kotlin/com/beyondeye/reduks/DispatcherFn.kt
+++ reduks/src/main/kotlin/com/beyondeye/reduks/DispatcherFn.kt
@@ -2,21 +2,56 @@ package com.beyondeye.reduks
 
 import java.lang.ref.WeakReference
 
+interface DispatcherFn {
+    val fn: ((action: Any) -> Any)?
+    /**
+     * just an alias for [invoke] so that we can use [DispatcherFn] as it was
+     * the source [Reduks] object
+     */
+    fun dispatch(action: Any): Any?
+
+    operator fun invoke(action: Any): Any?
+
+    companion object {
+        /**
+         * TODO: note that the weak reference feature has been disabled for now
+         * because it caused problems: for some reason it becomes immediately null: need to check better why
+         */
+        fun instance(fn: (action: Any) -> Any, isWeakRef: Boolean=false) = if (isWeakRef) DispatcherFn_weakref(fn) else DispatcherFn_ref(fn)
+    }
+}
+
 /**
  * class that hold a weak reference to a dispatcher function,
  * it is useful in android when we want to allow to dispatch actions
  * from objects that do not have direct access to the parent activity,
  * while avoiding potential memory leaks, and also encapsulating the
- * data type of a dispatcher function
+ * data type of a dispatcher function.
  */
-class DispatcherFn(fn_:(action:Any)->Any) {
-    val fn=WeakReference(fn_)
+class DispatcherFn_weakref(fn_:(action:Any)->Any) : DispatcherFn {
+    val fnw=WeakReference(fn_)
+    override val fn: ((action: Any) -> Any)?
+        get() = fnw.get()
+
+    override fun dispatch(action:Any):Any?=invoke(action)
+    override operator fun  invoke(action:Any):Any? {
+        return fnw.get()?.run {invoke(action) }
+    }
+}
+
+
+/**
+ * another implementation of [DispatcherFn] that keep a regular reference (not weak reference) to
+ * the dispatcher
+ */
+class DispatcherFn_ref(fn_:(action:Any)->Any) : DispatcherFn {
+    override val fn: ((action: Any) -> Any)?=fn_
     /**
      * just an alias for [invoke] so that we can use [DispatcherFn] as it was
      * the source [Reduks] object
      */
-    fun dispatch(action:Any):Any?=invoke(action)
-    operator fun  invoke(action:Any):Any? {
-        return fn.get()?.run {invoke(action) }
+    override fun dispatch(action:Any):Any?=invoke(action)
+    override operator fun  invoke(action:Any):Any? {
+        return fn.invoke(action)
     }
 }
