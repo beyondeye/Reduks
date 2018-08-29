718a12d57ca676c28d1db853813b3c4cdc3bb52d
beyondeye <dario-e@beyond-eye.com>
2018-08-29 12:59:05 +0300

    simplified AsyncStore (now logic is very similar to KovenantStore). this also fixes the issue that retrieving the store state after dispatching an action was not garanteed to return the state after processing of the action. Now this is garanteed (as it was in KovenantStore)

--- reduks-core-async/src/main/kotlin/com/beyondeye/reduks/experimental/AsyncStore.kt
+++ reduks-core-async/src/main/kotlin/com/beyondeye/reduks/experimental/AsyncStore.kt
@@ -1,17 +1,12 @@
 package com.beyondeye.reduks.experimental
 
-//import android.util.Log
 import com.beyondeye.reduks.*
 import com.beyondeye.reduks.experimental.middlewares.AsyncActionMiddleWare
 import com.beyondeye.reduks.middlewares.ThunkMiddleware
 import com.beyondeye.reduks.middlewares.applyMiddleware
-import kotlinx.coroutines.experimental.DefaultDispatcher
-import kotlinx.coroutines.experimental.channels.SendChannel
-import kotlinx.coroutines.experimental.channels.actor
-import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.*
 //import kotlinx.coroutines.experimental.newSingleThreadContext
 import kotlin.coroutines.experimental.CoroutineContext
-
 
 /**
  * Store that use kotlin coroutine channels for notifying asynchronously to store subscribers about
@@ -24,7 +19,7 @@ import kotlin.coroutines.experimental.CoroutineContext
 //
 class AsyncStore<S>(initialState: S, private var reducer: Reducer<S>,
                     subscribeContext: CoroutineContext,
-                    reduceContext: CoroutineContext =DefaultDispatcher
+                    val reduceContext: CoroutineContext =DefaultDispatcher
                     ) : Store<S> {
     class Creator<S>(
                      val subscribeContext: CoroutineContext,
@@ -38,34 +33,15 @@ class AsyncStore<S>(initialState: S, private var reducer: Reducer<S>,
                 res.applyMiddleware(ThunkMiddleware(),AsyncActionMiddleWare())
         }
     }
-    override var state:S=initialState
-            private set
-    private var subscribers= listOf<StoreSubscriber<S>>()
-    private val subscriberNotifierActor: SendChannel<S>
-    private val reducerActor: SendChannel<Any>
-    init {
-        subscriberNotifierActor = startSubscriberNotifierActor(subscribeContext)
-        reducerActor = startReducerActor(subscriberNotifierActor,reduceContext)
-    }
-    //NOTE THAT IF THE ObserveContext is a single thread(the ui thread)
-    // then subscribers will be notified sequentially of state changes in the correct order
-    private fun startSubscriberNotifierActor(c: CoroutineContext) = actor<S>(c) {
-        for(updatedState in channel) { //iterate over incoming state updates
-            notifySubscribers()
-        }
-    }
-    private fun startReducerActor(subscriberNotifierChannel:SendChannel<S>,c: CoroutineContext =DefaultDispatcher)= actor<Any>(c) {
-        for(action in channel) { //iterate over incoming actions
-            var newState=state
-            try {
-                newState = reducer.reduce(state, action) //return newState
-            } catch (e:Exception) {
-                //Log.w("rdks","exception in reducer while processing $action: ${e.toString()}")
-            }
-            state=newState
-            subscriberNotifierChannel.send(newState)
 
+    override var errorLogFn: ((String) -> Unit)?=null
+    private var deferredState: Deferred<S> = async { initialState }
+    override val state:S
+        get() = runBlocking(reduceContext) {
+            deferredState.await()
         }
+    private var subscribers= listOf<StoreSubscriber<S>>()
+    init {
     }
 
     override fun replaceReducer(reducer: Reducer<S>) {
@@ -75,9 +51,31 @@ class AsyncStore<S>(initialState: S, private var reducer: Reducer<S>,
 
     private val mainDispatcher = object : Middleware<S> {
         override fun dispatch(store: Store<S>, nextDispatcher:  (Any)->Any, action: Any): Any {
-            launch(DefaultDispatcher) {
-                reducerActor.send(action)
+            //get a reference of current deferred so that we make sure that all reduce action are actually executed in the correct order
+            val curDeferredState = deferredState
+            //update deferredState with result of async job of running the reducer
+            deferredState = async(reduceContext) {
+                val startState = curDeferredState.await()
+                val newState = try {
+                    reducer.reduce(startState, action) //return newState
+                } catch (e: Throwable) {
+                    ReduksInternalLogUtils.reportErrorInReducer(this@AsyncStore, e)
+                    startState
+                }
+                newState
             }
+            //after creating the new deferredState, handle notification of subscribers once this new
+            //deferredState is resolved
+            val nextDeferredState=deferredState
+            launch(reduceContext) {
+                nextDeferredState.await()
+                //NOTE THAT IF THE ObserveContext is a single thread(the ui thread)
+                // then subscribers will be notified sequentially of state changes in the correct order
+                withContext(subscribeContext) {
+                    notifySubscribers()
+                }
+            }
+
             return action
         }
     }
@@ -87,7 +85,7 @@ class AsyncStore<S>(initialState: S, private var reducer: Reducer<S>,
             try {
                 sub.onStateChange()
             } catch(e:Exception) {
-               // Log.w(SimpleStore.redukstag,"exception while notifying state change to subscriber: ${e.toString()}")
+                ReduksInternalLogUtils.reportErrorInSubscriber(this,e)
             }
         }
     }
@@ -102,10 +100,6 @@ class AsyncStore<S>(initialState: S, private var reducer: Reducer<S>,
                  { it -> it}, //null dispatcher that ends the chain
                 action)
     }
-    fun stopActors() {
-        subscriberNotifierActor.close()
-        reducerActor.close()
-    }
 
     override fun subscribe(storeSubscriber: StoreSubscriber<S>): StoreSubscription {
         synchronized(subscribers) {
@@ -118,6 +112,13 @@ class AsyncStore<S>(initialState: S, private var reducer: Reducer<S>,
                 }
             }
         }
+    }
+
+    /**
+     * this method does nothing: we don't use actors in the current version of asyncstore
+     */
+    fun stopActors() {
+
     }
 
 }
