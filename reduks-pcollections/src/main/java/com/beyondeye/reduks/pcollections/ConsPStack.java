6e2726b78e9307722aa0df033d22126c00b9b30a
Dario Elyasy <dario-e@eye-beyond.com>
2020-06-04 09:41:27 +0300

    updated integrated PCollections library to latest (3.1.3)

--- reduks-pcollections/src/main/java/com/beyondeye/reduks/pcollections/ConsPStack.java
+++ reduks-pcollections/src/main/java/com/beyondeye/reduks/pcollections/ConsPStack.java
@@ -1,214 +1,251 @@
+/*
+ * Copyright (c) 2008 Harold Cooper. All rights reserved.
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ */
+
 package com.beyondeye.reduks.pcollections;
 
+import java.io.Serializable;
 import java.util.AbstractSequentialList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.ListIterator;
 
-
-
 /**
- * 
  * A simple persistent stack of non-null values.
- * <p>
- * This implementation is thread-safe (assuming Java's AbstractSequentialList is thread-safe),
- * although its iterators may not be.
- * 
- * @author harold
  *
+ * <p>This implementation is thread-safe (assuming Java's AbstractSequentialList is thread-safe),
+ * although its iterators may not be.
+ *
+ * @author harold
  * @param <E>
  */
-public final class ConsPStack<E> extends AbstractSequentialList<E> implements PStack<E> {
-//// STATIC FACTORY METHODS ////
-	private static final ConsPStack<Object> EMPTY = new ConsPStack<Object>();
-	
-	/**
-	 * @param <E>
-	 * @return an empty stack
-	 */
-	@SuppressWarnings("unchecked")
-	public static <E> ConsPStack<E> empty() {
-		return (ConsPStack<E>)EMPTY; }
-	
-	/**
-	 * @param <E>
-	 * @param e
-	 * @return empty().plus(e)
-	 */
-	public static <E> ConsPStack<E> singleton(final E e) {
-		return ConsPStack.<E>empty().plus(e); }
-	
-	/**
-	 * @param <E>
-	 * @param list
-	 * @return a stack consisting of the elements of list in the order of list.iterator()
-	 */
-	@SuppressWarnings("unchecked")
-	public static <E> ConsPStack<E> from(final Collection<? extends E> list) {
-		if(list instanceof ConsPStack)
-			return (ConsPStack<E>)list; //(actually we only know it's ConsPStack<? extends E>)
-									// but that's good enough for an immutable
-									// (i.e. we can't mess someone else up by adding the wrong type to it)
-		return ConsPStack.<E>from(list.iterator());
-	}
-	
-	private static <E> ConsPStack<E> from(final Iterator<? extends E> i) {
-		if(!i.hasNext()) return empty();
-		E e = i.next();
-		return ConsPStack.<E>from(i).plus(e);
-	}
+public final class ConsPStack<E> extends AbstractSequentialList<E>
+    implements PStack<E>, Serializable {
 
-	
-//// PRIVATE CONSTRUCTORS ////
-	private final E first; private final ConsPStack<E> rest;
-	private final int size;
-	// not externally instantiable (or subclassable):
-	private ConsPStack() { // EMPTY constructor
-		if(EMPTY!=null)
-			throw new RuntimeException("empty constructor should only be used once");
-		size = 0; first=null; rest=null;
-	}
-	private ConsPStack(final E first, final ConsPStack<E> rest) {
-		this.first = first; this.rest = rest;
-		
-		size = 1 + rest.size;
-	}
-	
-	
-//// REQUIRED METHODS FROM AbstractSequentialList ////
-	@Override
-	public int size() {
-		return size; }
-	
-	@Override
-	public ListIterator<E> listIterator(final int index) {
-		if(index<0 || index>size) throw new IndexOutOfBoundsException();
-		
-		return new ListIterator<E>() {
-			int i = index;
-			ConsPStack<E> next = subList(index);
+  private static final long serialVersionUID = 1L;
 
-			public boolean hasNext() {
-				return next.size>0; }
-			public boolean hasPrevious() {
-				return i>0; }
-			public int nextIndex() {
-				return index; }
-			public int previousIndex() {
-				return index-1; }
-			public E next() {
-				E e = next.first;
-				next = next.rest;
-				return e;
-			}
-			public E previous() {
-				System.err.println("ConsPStack.listIterator().previous() is inefficient, don't use it!");
-				next = subList(index-1); // go from beginning...
-				return next.first;
-			}
+  //// STATIC FACTORY METHODS ////
+  private static final ConsPStack<Object> EMPTY = new ConsPStack<Object>();
 
-			public void add(final E o) {
-				throw new UnsupportedOperationException(); }
-			public void remove() {
-				throw new UnsupportedOperationException(); }
-			public void set(final E o) {
-				throw new UnsupportedOperationException(); }
-		};
-	}
+  /**
+   * @param <E>
+   * @return an empty stack
+   */
+  @SuppressWarnings("unchecked")
+  public static <E> ConsPStack<E> empty() {
+    return (ConsPStack<E>) EMPTY;
+  }
 
+  /**
+   * @param <E>
+   * @param e
+   * @return empty().plus(e)
+   */
+  public static <E> ConsPStack<E> singleton(final E e) {
+    return ConsPStack.<E>empty().plus(e);
+  }
 
-//// OVERRIDDEN METHODS FROM AbstractSequentialList ////
-	@Override
-	public ConsPStack<E> subList(final int start, final int end) {
-		if(start<0 || end>size || start>end)
-			throw new IndexOutOfBoundsException();
-		if(end==size) // want a substack
-			return subList(start); // this is faster
-		if(start==end) // want nothing
-			return empty();
-		if(start==0) // want the current element
-			return new ConsPStack<E>(first, rest.subList(0, end-1));
-		// otherwise, don't want the current element:
-		return rest.subList(start-1, end-1);
-	}
-	
-	
-//// IMPLEMENTED METHODS OF PStack ////
-	public ConsPStack<E> plus(final E e) {
-		return new ConsPStack<E>(e, this);
-	}
-	
-	public ConsPStack<E> plusAll(final Collection<? extends E> list) {
-		ConsPStack<E> result = this;
-		for(E e : list)
-			result = result.plus(e);
-		return result;
-	}
+  /**
+   * @param <E>
+   * @param list
+   * @return a stack consisting of the elements of list in the order of list.iterator()
+   */
+  @SuppressWarnings("unchecked")
+  public static <E> ConsPStack<E> from(final Collection<? extends E> list) {
+    if (list instanceof ConsPStack)
+      return (ConsPStack<E>) list; // (actually we only know it's ConsPStack<? extends E>)
+    // but that's good enough for an immutable
+    // (i.e. we can't mess someone else up by adding the wrong type to it)
+    return ConsPStack.<E>from(list.iterator());
+  }
 
-	public ConsPStack<E> plus(final int i, final E e) {
-		if(i<0 || i>size)
-			throw new IndexOutOfBoundsException();
-		if(i==0) // insert at beginning
-			return plus(e);
-		return new ConsPStack<E>(first, rest.plus(i-1, e));
-	}
+  private static <E> ConsPStack<E> from(final Iterator<? extends E> i) {
+    if (!i.hasNext()) return empty();
+    E e = i.next();
+    return ConsPStack.<E>from(i).plus(e);
+  }
 
-	public ConsPStack<E> plusAll(final int i, final Collection<? extends E> list) {
-		// TODO inefficient if list.isEmpty()
-		if(i<0 || i>size)
-			throw new IndexOutOfBoundsException();
-		if(i==0)
-			return plusAll(list);
-		return new ConsPStack<E>(first, rest.plusAll(i-1, list));
-	}
-	
-	public ConsPStack<E> minus(final Object e) {
-		if(size==0)
-			return this;
-		if(first.equals(e)) // found it
-			return rest; // don't recurse (only remove one)
-		// otherwise keep looking:
-		ConsPStack<E> newRest = rest.minus(e);
-		if(newRest==rest) return this;
-		return new ConsPStack<E>(first, newRest);
-	}
+  //// PRIVATE CONSTRUCTORS ////
+  private final E first;
+  private final ConsPStack<E> rest;
+  private final int size;
+  // not externally instantiable (or subclassable):
+  private ConsPStack() { // EMPTY constructor
+    if (EMPTY != null) throw new RuntimeException("empty constructor should only be used once");
+    size = 0;
+    first = null;
+    rest = null;
+  }
 
-	public ConsPStack<E> minus(final int i) {
-		if (i < 0 || i >= size)
-			throw new IndexOutOfBoundsException("Index: " + i + "; size: " + size);
-		else if (i == 0)
-			return rest;
-		else return new ConsPStack<E>(first, rest.minus(i-1));
-	}
+  private ConsPStack(final E first, final ConsPStack<E> rest) {
+    this.first = first;
+    this.rest = rest;
 
-	public ConsPStack<E> minusAll(final Collection<?> list) {
-		if(size==0)
-			return this;
-		if(list.contains(first)) // get rid of current element
-			return rest.minusAll(list); // recursively delete all
-		// either way keep looking:
-		ConsPStack<E> newRest = rest.minusAll(list);
-		if(newRest==rest) return this;
-		return new ConsPStack<E>(first, newRest);
-	}
-	
-	public ConsPStack<E> with(final int i, final E e) {
-		if(i<0 || i>=size)
-			throw new IndexOutOfBoundsException();
-		if(i==0) {
-			if(first.equals(e)) return this;
-			return new ConsPStack<E>(e, rest);
-		}
-		ConsPStack<E> newRest = rest.with(i-1, e);
-		if(newRest==rest) return this;
-		return new ConsPStack<E>(first, newRest);
-	}
+    size = 1 + rest.size;
+  }
 
-	public ConsPStack<E> subList(final int start) {
-		if(start<0 || start>size)
-			throw new IndexOutOfBoundsException();
-		if(start==0)
-			return this;
-		return rest.subList(start-1);
-	}
+  //// REQUIRED METHODS FROM AbstractSequentialList ////
+  @Override
+  public int size() {
+    return size;
+  }
+
+  @Override
+  public ListIterator<E> listIterator(final int index) {
+    if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+
+    return new ListIterator<E>() {
+      int i = index;
+      ConsPStack<E> next = subList(index);
+
+      public boolean hasNext() {
+        return next.size > 0;
+      }
+
+      public boolean hasPrevious() {
+        return i > 0;
+      }
+
+      public int nextIndex() {
+        return i;
+      }
+
+      public int previousIndex() {
+        return i - 1;
+      }
+
+      public E next() {
+        E e = next.first;
+        next = next.rest;
+        i++;
+        return e;
+      }
+
+      public E previous() {
+        System.err.println("ConsPStack.listIterator().previous() is inefficient, don't use it!");
+        next = subList(--i); // go from beginning...
+        return next.first;
+      }
+
+      public void add(final E o) {
+        throw new UnsupportedOperationException();
+      }
+
+      public void remove() {
+        throw new UnsupportedOperationException();
+      }
+
+      public void set(final E o) {
+        throw new UnsupportedOperationException();
+      }
+    };
+  }
+
+  //// OVERRIDDEN METHODS FROM AbstractSequentialList ////
+  @Override
+  public ConsPStack<E> subList(final int start, final int end) {
+    if (start < 0 || end > size || start > end) throw new IndexOutOfBoundsException();
+    if (start == end) return empty();
+    if (start > 0) return subList(start).subList(0, end - start); // remove from beginning
+    if (end == size) return this;
+
+    // remove from end (by popping off until end, and then pushing back on)
+    ConsPStack<E> reversed = empty();
+    for (final E e : this) {
+      if (reversed.size == end) break;
+      reversed = reversed.plus(e);
+    }
+    return this.<E>empty().plusAll(reversed); // plusAll reverses again
+  }
+
+  //// IMPLEMENTED METHODS OF PStack ////
+  public ConsPStack<E> plus(final E e) {
+    return new ConsPStack<E>(e, this);
+  }
+
+  public ConsPStack<E> plusAll(final Collection<? extends E> list) {
+    ConsPStack<E> result = this;
+    for (E e : list) result = result.plus(e);
+    return result;
+  }
+
+  public ConsPStack<E> plus(final int i, final E e) {
+    return plusAll(i, singleton(e));
+  }
+
+  public ConsPStack<E> plusAll(final int i, final Collection<? extends E> list) {
+    if (i < 0 || i > size) throw new IndexOutOfBoundsException();
+    if (list.isEmpty()) return this;
+
+    ConsPStack<E> reversed = empty();
+    ConsPStack<E> suffix = this;
+    while (reversed.size < i) {
+      reversed = reversed.plus(suffix.first);
+      suffix = suffix.rest;
+    }
+    return suffix.plusAll(list).plusAll(reversed); // plusAll reverses again
+  }
+
+  public ConsPStack<E> minus(final Object e) {
+    final int i = indexOf(e);
+    if (i == -1) return this;
+    return minus(i);
+  }
+
+  public ConsPStack<E> minus(final int i) {
+    if (i < 0 || i >= size) throw new IndexOutOfBoundsException("Index: " + i + "; size: " + size);
+
+    ConsPStack<E> reversed = empty();
+    ConsPStack<E> suffix = this;
+    while (reversed.size <= i) {
+      reversed = reversed.plus(suffix.first);
+      suffix = suffix.rest;
+    }
+    return suffix.plusAll(reversed.rest); // plusAll reverses again
+  }
+
+  public ConsPStack<E> minusAll(final Collection<?> list) {
+    // TODO wrap list in a Set to speed up contains()?
+    if (list.isEmpty()) return this;
+
+    ConsPStack<E> kept = empty();
+    ConsPStack<E> searching = this;
+    ConsPStack<E> reversed = kept;
+    ConsPStack<E> suffix = searching;
+    while (searching.size > 0) {
+      final E e = searching.first;
+      searching = searching.rest;
+      if (list.contains(e)) {
+        // checkpoint only when we actually remove something, so we can reuse existing suffix stack
+        reversed = kept;
+        suffix = searching;
+      } else kept = kept.plus(e);
+    }
+    return suffix.plusAll(reversed); // plusAll reverses again
+  }
+
+  public ConsPStack<E> with(final int i, final E e) {
+    if (i < 0 || i >= size) throw new IndexOutOfBoundsException();
+
+    ConsPStack<E> reversed = empty();
+    ConsPStack<E> suffix = this;
+    while (reversed.size <= i) {
+      reversed = reversed.plus(suffix.first);
+      suffix = suffix.rest;
+    }
+    return suffix.plus(e).plusAll(reversed.rest); // plusAll reverses again
+  }
+
+  public ConsPStack<E> subList(int start) {
+    if (start < 0 || start > size) throw new IndexOutOfBoundsException();
+
+    ConsPStack<E> s = this;
+    while (start > 0) {
+      s = s.rest;
+      start--;
+    }
+    return s;
+  }
 }
--- reduks-pcollections/src/test/java/com/beyondeye/reduks/pcollections/OrderedPSet.java
+++ reduks-pcollections/src/test/java/com/beyondeye/reduks/pcollections/OrderedPSet.java
@@ -1,88 +1,94 @@
+/*
+ * Copyright (c) 2008 Harold Cooper. All rights reserved.
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ */
+
 package com.beyondeye.reduks.pcollections;
 
+import java.io.Serializable;
 import java.util.AbstractSet;
 import java.util.Collection;
 import java.util.Iterator;
 
-public class OrderedPSet<E> extends AbstractSet<E> implements POrderedSet<E> {
-	private static final OrderedPSet<Object> EMPTY = new OrderedPSet<Object>(
-			Empty.set(), Empty.vector());
+public class OrderedPSet<E> extends AbstractSet<E> implements POrderedSet<E>, Serializable {
 
-	@SuppressWarnings("unchecked")
-	public static <E> OrderedPSet<E> empty() {
-		return (OrderedPSet<E>) EMPTY;
-	}
+  private static final long serialVersionUID = 1L;
 
-	@SuppressWarnings("unchecked")
-	public static <E> OrderedPSet<E> from(final Collection<? extends E> list) {
-		if (list instanceof OrderedPSet)
-			return (OrderedPSet<E>) list;
-		return OrderedPSet.<E> empty().plusAll(list);
-	}
+  private static final OrderedPSet<Object> EMPTY =
+      new OrderedPSet<Object>(Empty.set(), Empty.vector());
 
-	public static <E> OrderedPSet<E> singleton(final E e) {
-		return OrderedPSet.<E> empty().plus(e);
-	}
+  @SuppressWarnings("unchecked")
+  public static <E> OrderedPSet<E> empty() {
+    return (OrderedPSet<E>) EMPTY;
+  }
 
-	private PSet<E> contents;
-	private PVector<E> order;
+  @SuppressWarnings("unchecked")
+  public static <E> OrderedPSet<E> from(final Collection<? extends E> list) {
+    if (list instanceof OrderedPSet) return (OrderedPSet<E>) list;
+    return OrderedPSet.<E>empty().plusAll(list);
+  }
 
-	private OrderedPSet(PSet<E> c, PVector<E> o) {
-		contents = c;
-		order = o;
-	}
+  public static <E> OrderedPSet<E> singleton(final E e) {
+    return OrderedPSet.<E>empty().plus(e);
+  }
 
-	@Override
-	public OrderedPSet<E> plus(E e) {
-		if (contents.contains(e))
-			return this;
-		return new OrderedPSet<E>(contents.plus(e), order.plus(e));
-	}
+  private PSet<E> contents;
+  private PVector<E> order;
 
-	@Override
-	public OrderedPSet<E> plusAll(Collection<? extends E> list) {
-		OrderedPSet<E> s = this;
-		for (E e : list) {
-			s = s.plus(e);
-		}
-		return s;
-	}
+  private OrderedPSet(PSet<E> c, PVector<E> o) {
+    contents = c;
+    order = o;
+  }
 
-	@Override
-	public OrderedPSet<E> minus(Object e) {
-		if (!contents.contains(e))
-			return this;
-		return new OrderedPSet<E>(contents.minus(e), order.minus(e));
-	}
+  @Override
+  public OrderedPSet<E> plus(E e) {
+    if (contents.contains(e)) return this;
+    return new OrderedPSet<E>(contents.plus(e), order.plus(e));
+  }
 
-	@Override
-	public OrderedPSet<E> minusAll(Collection<?> list) {
-		OrderedPSet<E> s = this;
-		for (Object e : list) {
-			s = s.minus(e);
-		}
-		return s;
-	}
+  @Override
+  public OrderedPSet<E> plusAll(Collection<? extends E> list) {
+    OrderedPSet<E> s = this;
+    for (E e : list) {
+      s = s.plus(e);
+    }
+    return s;
+  }
 
-	@Override
-	public Iterator<E> iterator() {
-		return order.iterator();
-	}
+  @Override
+  public OrderedPSet<E> minus(Object e) {
+    if (!contents.contains(e)) return this;
+    return new OrderedPSet<E>(contents.minus(e), order.minus(e));
+  }
 
-	@Override
-	public int size() {
-		return contents.size();
-	}
+  @Override
+  public OrderedPSet<E> minusAll(Collection<?> list) {
+    OrderedPSet<E> s = this;
+    for (Object e : list) {
+      s = s.minus(e);
+    }
+    return s;
+  }
 
-	@Override
-	public E get(int index) {
-		return order.get(index);
-	}
+  @Override
+  public Iterator<E> iterator() {
+    return order.iterator();
+  }
 
-	@Override
-	public int indexOf(Object o) {
-		if (!contents.contains(o))
-			return -1;
-		return order.indexOf(o);
-	}
+  @Override
+  public int size() {
+    return contents.size();
+  }
+
+  @Override
+  public E get(int index) {
+    return order.get(index);
+  }
+
+  @Override
+  public int indexOf(Object o) {
+    if (!contents.contains(o)) return -1;
+    return order.indexOf(o);
+  }
 }
